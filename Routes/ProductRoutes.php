6ef945315430bcdbbb3ae7d6614c77ffad5feb61
sultanprabowo90@gmail.com <sultanprabowo90@gmail.com>
2024-11-21 21:41:37 +0700

    third commit

---
+++ Routes/ProductRoutes.php
@@ -0,0 +1,55 @@
+<?php
+namespace app\Routes;
+
+include __DIR__ . '/../Controller/ProductController.php';
+
+use app\Controller\ProductController;
+
+class ProductRoutes
+  {
+  public function handle($method, $path)
+  {
+    $basePath = '/app/app';
+    if (strpos($path, $basePath) === 0) {
+        $path = substr($path, strlen($basePath));
+    }
+
+    if ($method == "GET" && $path == '/api/product') {
+      $controller = new ProductController();
+      echo $controller->index();
+    }
+
+    if ($method == "GET" && strpos($path, "/api/product/") == 0) {
+      $pathParts = explode("/", $path);
+      $id = $pathParts[count($pathParts) -1];
+
+      $controller = new ProductController();
+      echo $controller->getById($id);
+    }
+
+    if ($method == "POST" && $path == "/api/product/") {
+      $controller = new ProductController();
+      echo $controller->insert();
+    }
+
+    if ($method == "PUT" && strpos($path, "/api/product/") == 0) {
+      $pathParts = explode("/", $path);
+      $id = $pathParts[count($pathParts) -1];
+
+      $controller = new ProductController();
+      echo $controller->update($id);
+    }
+
+    if ($method == "DELETE" && strpos($path, "/api/product/") == 0) {
+      $pathParts = explode("/", $path);
+      $id = $pathParts[count($pathParts) -1];
+
+      $controller = new ProductController();
+      echo $controller->delete($id);
+    }
+  }
+}
+
+
+
+ ?>
